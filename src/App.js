d17f3e32f1b7e2e27a691004e68ced50a0eb0172
Jander Gustavo Alves <jander.gustavo@gmail.com>
2022-04-27 16:26:43 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter as Router,  Switch,  Route,  Link} from "react-router-dom";
+import Tabela from './components/tabela';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+  return (          
+
+      <div style={{width: '100%'}}>
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>           
+            <Tabela/>
+        </div>      
+      </div>
   );
 }
 
